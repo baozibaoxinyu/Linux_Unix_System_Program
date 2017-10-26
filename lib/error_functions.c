3824ad2f474b43c847cad33709398c61716cd0ea
baozibaoxinyu <26892842+baozibaoxinyu@users.noreply.github.com>
2017-10-26 22:50:37 +0800

    Add files via upload

--- lib/error_functions.c
+++ lib/error_functions.c
@@ -25,4 +25,73 @@ static void outputError(Boolean useErr,int err,Boolean flushStdout,
 #define BUF_SIZE 500
 	char buf[BUF_SIZE],userMsg[BUF_SIZE],errText[BUF_SIZE];
 	vsnprintf(userMsg,BUF_SIZE,format,ap);
+	if(useErr)
+		snprintf(errText,BUF_SIZE," [%s %s]",
+			(err > 0 && err <= MAX_ENAME)?
+			ename[err]:"?UNKNOW?",stderror(err));
+	else
+		snprintf(errText,BUF_SIZE,":");
+	snprintf(buf,BUF_SIZE,"ERROR%s %s\n",errText,userMsg);
+	if(flushStdout)
+		fflush(stdout);		/* Flush any pending stdout */
+	fputs(buf,stderr);
+	fflush(stderr);			/* In case stderr is not line-buffered */
 }
+void errMsg(const char* format,...){
+	va_list argList;
+	int savedErrno;
+	savedErrno = errno;		/* In case we change it here */
+	va_start(argList,format);
+	outputError(TRUE,errno,TRUE,format,argList);
+	va_end(argList);
+	errno = savedErrno;
+}
+void errExit(const char* format,...){
+	va_list argList;
+	va_start(argList,format);
+	outputError(TRUE,errno,TRUE,format,arglist);
+	va_end(argList);
+	terminate(TRUE);
+}
+void err_exit(const char* format,...){
+	va_list argList;
+	va_start(argList,format);
+	outputError(TRUE,errno,FALSE,format,argList);
+	va_end(argList);
+	terminate(FALSE);
+}
+void errExitEN(int errnum,const char* format,...){
+	va_list argList;
+	va_start(argList,format);
+	outputError(TRUE,errnum,TRUE,format,argList);
+	va_end(argList);
+	terminate(TRUE);
+}
+void fatal(const char* format,...){
+	va_list argList;
+	va_start(argList,format);
+	outputError(FALSE,0,TRUE,format,argList);
+	va_end(argList);
+	terminate(TRUE);
+}
+void usageErr(const char* format,...){
+	va_list argList;
+	fflush(stdout);			/* Flust any pending stdout */
+	fprintf(stderr,"Usage: ");
+	va_start(argList,format);
+	vfprintf(stderr,format,argList);
+	va_end(argList);
+	fflush(stderr);			/* In case stderr is not line-buffered */
+	exit(EXIT_FAILURE);
+}
+void cmdLineErr(cosnt char* format,...){
+	va_list argList;
+        fflush(stdout);                 /* Flust any pending stdout */
+        fprintf(stderr,"Usage: ");
+        va_start(argList,format);
+        vfprintf(stderr,format,argList);
+        va_end(argList);
+        fflush(stderr);			/* In case stderr is not line-buffered */
+        exit(EXIT_FAILURE);
+}
+
